6d0bc607e770a9dd4417534e49b40f318d326749
sbonele <sbonelemiya@gmail.com>
2023-05-03 13:59:24 +0200

    refresh

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,7 @@
+# SSH configuration file to connect to server without password
+
+exec { 'echo':
+  path    => 'usr/bin:/bin',
+  command => 'echo "    IdentityFile ~/.ssh/school\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
+  returns => [0,1],
+}
